81ba373dea819ecf1322944c0debcfe5fd4c4171
Andrew Greene <andrewgreene120@gmail.com>
2022-09-19 20:19:30 -0400

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
+
 def hello_world(city, state):
     print(f'Hello World! {city}, {state}!')
 
 
 def main():
     hello_world('NYC','New York')
-=======
-
->>>>>>> 1b3e1965188b9a7a8e5f3da7373b94f580f3182f
-
 
 main()
